859bbc831f33694cdc2fe971daa089b16e7566bf
Dương Thanh Phụng <20130099@st.hcmuaf.edu.vn>
2023-05-23 14:46:52 +0700

    Sửa port để deploy

--- main.js
+++ main.js
@@ -28,6 +28,6 @@ server.use((req, res, next) => {
 // Use default router
 server.use('/api', router)
 const PORT = process.env.PORT || 3000;
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log('JSON Server is running')
 })
